bf33b2eb68f803885bf63223a58fd9f1609cc30e
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-06-11 00:07:16 +0300

    Fix configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -58,14 +58,15 @@ export default tseslint.config(
           next: "return",
         },
       ],
-      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-floating-promises": "warn",
       "@typescript-eslint/no-unsafe-argument": "warn",
       "@typescript-eslint/no-unsafe-assignment": "warn",
-      "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-member-access": "warn",
-      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-unsafe-call": "warn",
       "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-empty-interface": "off",
       "@typescript-eslint/no-unused-vars": [
         "warn",
         {
