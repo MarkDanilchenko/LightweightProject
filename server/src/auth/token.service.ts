01ba55825cb5123a3739058f8cad8820fa21dfa8
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-06-18 00:31:08 +0300

    Add tokens service

---
+++ server/src/auth/token.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { AppConfiguration } from "../configs/interfaces/appConfiguration.interface.js";
+
+@Injectable()
+export default class TokenService {
+  private readonly jwtAccessTokenExpirationTime: string;
+  private readonly jwtRefreshTokenExpirationTime: string;
+
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {
+    this.jwtAccessTokenExpirationTime = configService.get<AppConfiguration["jwtConfiguration"]["accessTokenExpiresIn"]>(
+      "jwtConfiguration.accessTokenExpiresIn",
+    )!;
+    this.jwtRefreshTokenExpirationTime = configService.get<
+      AppConfiguration["jwtConfiguration"]["refreshTokenExpiresIn"]
+    >("jwtConfiguration.refreshTokenExpiresIn")!;
+  }
+
+  async generateBothTokens(payload: Record<string, unknown>): Promise<{ accessToken: string; refreshToken: string }> {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.generateAccessToken(payload),
+      this.generateRefreshToken(payload),
+    ]);
+
+    return { accessToken, refreshToken };
+  }
+
+  async generateAccessToken(payload: Record<string, unknown>): Promise<string> {
+    const accessToken = await this.jwtService.signAsync(payload, {
+      expiresIn: this.jwtAccessTokenExpirationTime,
+    });
+
+    return accessToken;
+  }
+
+  async generateRefreshToken(payload: Record<string, unknown>): Promise<string> {
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      expiresIn: this.jwtRefreshTokenExpirationTime,
+    });
+
+    return refreshToken;
+  }
+
+  async verifyRefreshToken(token: string): Promise<void> {
+    const isVerified = await this.jwtService.verifyAsync(token);
+
+    return isVerified;
+  }
+}
