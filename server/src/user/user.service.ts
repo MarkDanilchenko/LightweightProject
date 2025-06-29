c4511f57a36ad75facc30503b54e8fb7f2c3da8f
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-06-29 19:39:58 +0300

    Add custom find method to userService

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { FindOneOptions, Repository } from "typeorm";
 import UserEntity from "@server/user/user.entity";
 
 @Injectable()
@@ -9,4 +9,18 @@ export default class UserService {
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
   ) {}
+
+  /**
+   * Finds a user entity by its primary key (user ID).
+   *
+   * @param userId - The ID of the user to find.
+   * @param options - Additional find options to customize the query.
+   *
+   * @returns A promise that resolves with the user entity if found, otherwise null.
+   */
+  async findByPk(userId: string, options?: FindOneOptions<UserEntity>): Promise<UserEntity | null> {
+    options = { ...options, where: { ...options?.where, id: userId } };
+
+    return this.userRepository.findOne(options);
+  }
 }
