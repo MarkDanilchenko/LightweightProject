99b0d4626c7553d3db162b82cbc2bd17b6fbee43
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-10-26 14:32:10 +0300

    Edit nodemailer instance

--- server/src/utils/nodemailer.ts
+++ server/src/utils/nodemailer.ts
@@ -1,15 +1,39 @@
-import { createTransport } from "nodemailer";
-import AppConfiguration from "@server/configs/app.configuration";
+import { createTransport, Transporter } from "nodemailer";
+import appConfiguration from "@server/configs/app.configuration";
+import { Logger } from "@nestjs/common";
+import AppConfiguration from "@server/configs/interfaces/appConfiguration.interfaces";
 
-const smtpConfiguration = AppConfiguration().smtpConfiguration;
+const logger = new Logger("Nodemailer");
+const smtpConfiguration: AppConfiguration["smtpConfiguration"] = appConfiguration().smtpConfiguration;
 
-const transporter = createTransport({
+if (
+  !smtpConfiguration?.host ||
+  !smtpConfiguration?.port ||
+  !smtpConfiguration?.username ||
+  !smtpConfiguration?.password ||
+  !smtpConfiguration?.from
+) {
+  logger.error("SMTP host or port or username or password or from are not configured.");
+
+  process.exit(1);
+}
+
+const transporter: Transporter = createTransport({
   host: smtpConfiguration.host,
   port: smtpConfiguration.port,
+  secure: smtpConfiguration.port === 465, // true for 465, false for other ports
   auth: {
     user: smtpConfiguration.username,
     pass: smtpConfiguration.password,
   },
+});
+
+transporter.verify((error): void => {
+  if (error) {
+    return logger.error("Error with SMTP configuration:", error);
+  }
+
+  return logger.log("SMTP configuration is valid.");
 });
 
 export default transporter;
