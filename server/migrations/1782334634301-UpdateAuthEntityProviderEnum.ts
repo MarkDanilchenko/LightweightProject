496fe68f45f28ed94db77bccbcae53a67dc620ff
MarkDanilchenko <danilchenko.mark50@gmail.com>
2026-06-27 16:42:46 +0300

    LWP-2: simplify migration down() method to avoid enum recreation conflicts

--- server/migrations/1782334634301-UpdateAuthEntityProviderEnum.ts
+++ server/migrations/1782334634301-UpdateAuthEntityProviderEnum.ts
@@ -12,19 +12,7 @@ export class UpdateAuthEntityProviderEnum1782334634301 implements MigrationInter
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX "public"."IDX_76f321fd7c6940ab57c5fed9cb"`);
-    await queryRunner.query(
-      `CREATE TYPE "public"."authentications_provider_enum_old" AS ENUM('local', 'google', 'keycloak', 'github')`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "authentications" ALTER COLUMN "provider" TYPE "public"."authentications_provider_enum_old" USING "provider"::"text"::"public"."authentications_provider_enum_old"`,
-    );
-    await queryRunner.query(`DROP TYPE "public"."authentications_provider_enum"`);
-    await queryRunner.query(
-      `ALTER TYPE "public"."authentications_provider_enum_old" RENAME TO "authentications_provider_enum"`,
-    );
-    await queryRunner.query(
-      `CREATE UNIQUE INDEX "IDX_76f321fd7c6940ab57c5fed9cb" ON "authentications" ("userId", "provider")`,
-    );
+    // The down() migration can not recreate the provider enum without 'yandex',
+    // because it fails if any rows already contain provider='yandex'.
   }
 }
